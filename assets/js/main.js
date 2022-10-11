e19a7af86fdfc0d8b805282ec4fd9ff8a3327b67
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-11 15:31:56 +0200

    email request

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/* Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto. */ 
+
+// STRUMENTI: const, prompt, array, 
+
+// Chiedi all’utente la sua email
+const email = prompt('Inserisci la tua email');
+// console.log(email);
+
+// controlla che sia nella lista di chi può accedere
+
+
+
+// for (let i = 0 )
